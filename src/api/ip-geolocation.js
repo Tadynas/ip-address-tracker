5ef51841958e285d2b72a416729b297c9821fe83
Tadas Gricius <griciustadas@gmail.com>
2020-11-16 15:56:01 +0200

    Added env variables

--- src/api/ip-geolocation.js
+++ src/api/ip-geolocation.js
@@ -1,41 +1,44 @@
 import axios from 'axios'
 
-// const geoIPurl = 'https://geo.ipify.org/api/v1'
+const geoIPurl = 'https://geo.ipify.org/api/v1'
 const ipURL = 'https://api.ipify.org'
-
-// const key = 'at_w1O8ZemDRkPa7LTNVv6OsBzGjQJwA'
+const key = process.env.REACT_APP_IPIFY_KEY
 
 export const fetchIPGeoData = async (address) => {
   try {
-    // const { data: { ip, isp, location } }  = await axios.get(`${geoIPurl}?apiKey=${key}&ipAddress=${address}`)
-    
-    const tempData = {
-      info: {
-        ip: '8.8.8.8',
-        location: 'Kalvariju g., Lithuania, 894684',
-        timezone: '+02:00',
-        isp: 'google.com'
-      },
-      coordinates: {
-        lat: 51.5074,
-        lng: 0.1278
+    let modifiedData;
+
+    if(process.env.NODE_ENV === 'development') {
+      modifiedData = {
+        info: {
+          ip: '8.8.8.8',
+          location: 'Kalvariju g., Lithuania, 894684',
+          timezone: '+02:00',
+          isp: 'google.com'
+        },
+        coordinates: {
+          lat: 51.5074,
+          lng: 0.1278
+        }
+      }
+    } else {
+      const { data: { ip, isp, location } }  = await axios.get(`${geoIPurl}?apiKey=${key}&ipAddress=${address}`)
+
+      modifiedData = {
+        info: {
+          ip: ip,
+          location: `${location.city}, ${location.country}, ${location.postalCode}`,
+          timezone: location.timezone,
+          isp: isp
+        },
+        coordinates: {
+          lat: location.lat,
+          lng: location.lng
+        }
       }
     }
 
-    // const modifiedData = {
-    //   info: {
-    //     ip: ip,
-    //     location: `${location.city}, ${location.country}, ${location.postalCode}`,
-    //     timezone: location.timezone,
-    //     isp: isp
-    //   },
-    //   coordinates: {
-    //     lat: location.lat,
-    //     lng: location.lng
-    //   }
-    // }
-
-    return tempData
+    return modifiedData
   } catch (error) {
     console.log(error)
   }
